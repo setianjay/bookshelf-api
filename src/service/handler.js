5dddf4dba4ec0f229dbf83a09b770f4dea57dd6f
Hari Setiaji <hari.setiaji2000@gmail.com>
2022-06-25 10:38:14 +0700

    Create a service to handle getting all book

--- src/service/handler.js
+++ src/service/handler.js
@@ -76,6 +76,38 @@ class Handler {
 
     return response;
   };
+
+  static getAllBookHandler = (request, h) => {
+    const { name, reading, finished } = request.query;
+    let filteredBooks = books;
+
+    if (name !== undefined) {
+      filteredBooks = books.filter((book) => book.name.toLowerCase().includes(name.toLowerCase()));
+    }
+
+    if (reading !== undefined) {
+      const isReading = reading === 1;
+      filteredBooks = books.filter((book) => book.reading === isReading);
+    }
+
+    if (finished !== undefined) {
+      const isFinished = finished === 1;
+      filteredBooks = books.filter((book) => book.finished === isFinished);
+    }
+
+    const response = h.response({
+      status: ResponseStatus.success,
+      data: {
+        books: filteredBooks.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+
+    return response;
+  };
 }
 
 module.exports = Handler;
--- src/service/routes.js
+++ src/service/routes.js
@@ -3,11 +3,17 @@ const RequestMethod = require('../constants/request-method');
 const RequestPath = require('../constants/request-path');
 
 class Routes {
-  static endpoint = [{
-    method: RequestMethod.post,
-    path: RequestPath.books,
-    handler: Handler.addBookHandler,
-  }];
+  static endpoint = [
+    {
+      method: RequestMethod.post,
+      path: RequestPath.books,
+      handler: Handler.addBookHandler,
+    },
+    {
+      method: RequestMethod.get,
+      path: RequestPath.books,
+      handler: Handler.getAllBookHandler,
+    }];
 }
 
 module.exports = Routes;
